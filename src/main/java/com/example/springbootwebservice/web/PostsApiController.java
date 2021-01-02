c90c2e9e38b96923292cbc03b4c3b32ae3f0a310
jane <sy4454@naver.com>
2021-01-02 18:40:23 +0900

    feat ⚡ post update & find function test

--- src/main/java/com/example/springbootwebservice/web/PostsApiController.java
+++ src/main/java/com/example/springbootwebservice/web/PostsApiController.java
@@ -1,11 +1,11 @@
 package com.example.springbootwebservice.web;
 
 import com.example.springbootwebservice.service.posts.PostsService;
+import com.example.springbootwebservice.web.dto.PostsResponseDto;
 import com.example.springbootwebservice.web.dto.PostsSaveRequestDto;
+import com.example.springbootwebservice.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
@@ -16,6 +16,16 @@ public class PostsApiController {
     @PostMapping("/api/v1/posts")
     public Long save(@RequestBody PostsSaveRequestDto requestDto) {
         return postsService.save(requestDto);
+    }
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
+        return postsService.update(id, requestDto);
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById (@PathVariable Long id) {
+        return postsService.findById(id);
     }
 
 }
