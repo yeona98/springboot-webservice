ee8d5d5c78330a484f141388ab35be0e5a4a0ece
jane <sy4454@naver.com>
2021-01-18 12:56:42 +0900

    refactor 🛠 using annotation for getting SessionUser info

---
+++ src/main/java/com/example/springbootwebservice/config/auth/LoginUserArgumentResolver.java
@@ -0,0 +1,37 @@
+package com.example.springbootwebservice.config.auth;
+
+import com.example.springbootwebservice.config.auth.dto.SessionUser;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.MethodParameter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+import javax.servlet.http.HttpSession;
+
+@RequiredArgsConstructor
+@Component
+public class LoginUserArgumentResolver implements HandlerMethodArgumentResolver {
+
+    private final HttpSession httpSession;
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+
+        boolean isLoginUserAnnotation = parameter.getParameterAnnotation(LoginUser.class) != null; //파라미터에 @LoginUser 붙어있는가
+        boolean isUserClass = SessionUser.class.equals(parameter.getParameterType()); //파라미터 클래스 타입이 SessionUser.class 인가
+
+        return isLoginUserAnnotation && isUserClass;
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter parameter,
+                                  ModelAndViewContainer mavContainer,
+                                  NativeWebRequest webRequest,
+                                  WebDataBinderFactory binderFactory) throws Exception {
+
+        return httpSession.getAttribute("user");
+    }
+}
--- src/main/java/com/example/springbootwebservice/web/IndexController.java
+++ src/main/java/com/example/springbootwebservice/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.example.springbootwebservice.web;
 
+import com.example.springbootwebservice.config.auth.LoginUser;
 import com.example.springbootwebservice.config.auth.dto.SessionUser;
 import com.example.springbootwebservice.service.posts.PostsService;
 import com.example.springbootwebservice.web.dto.PostsResponseDto;
@@ -19,9 +20,9 @@ public class IndexController {
     private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+
         if (user != null) {
             model.addAttribute("userName", user.getName());
         }
